fa2ce96aabffdc3498442190d31429cf95ee9f68
flybirdgroup <flybirdgroup@163.com>
2022-12-03 07:43:23 +0800

    usestate onClick with function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
-import {useState} from "react" 
+import { useState } from "react" ;
 // import {User} from './User'
 // import {Planets} from './Utils';
 
-
 const App = () => {
 
-  const [age,setAge] = useState(0);
-  
-  const IncreaseAge = () =>{
-    setAge(age + 1);
-  };
-  return <div className='App'>
-      {age}
-      <button onClick={IncreaseAge}>Increase Age</button>
+  const [count, setCount] = useState(0);
+
+  return (
+        <div className="App">
+        <button onClick={()=>{setCount(count+1)}}>Increase</button>
+        <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+        <button onClick={()=>{setCount(0)}}>Set to Zero</button>
+        {count}
          </div>
+         );
   
-};
-
-
+}
 export default App
