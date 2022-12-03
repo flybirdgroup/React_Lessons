fc760b67d9b78b463ecc56accb1a524b17ed2431
flybirdgroup <flybirdgroup@163.com>
2022-12-03 21:44:00 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -2,28 +2,54 @@ import React from 'react';
 import './App.css';
 // import {User} from './User'
 import {useState}from "react";
+import { TaskList } from './Utils';
 
 
 const App = () => {
-  const [todoList,setTodoList] = useState([])
-  const [newTask,setNewTask] = useState({})
-  const addTask =()=>{
 
+  const [todoList,setTodoList] = useState([])
+  const [newTask,setNewTask] = useState("")
+  
+  const addTask =()=>{
+    const task = {
+      id: todoList.length ===0? 1:todoList[todoList.length-1].id +1,
+      name: newTask,
+      completed: false
+    }
+    setTodoList([...todoList,task])
+    console.log(todoList)
+    
   }
   const inputTask = (event) =>{
-    setNewTask(
-      id=1,
-      name=event.target.value
-    )
+    setNewTask(event.target.value)
   }
+
+  const deleteTask = (taskid) =>{
+    setTodoList(todoList.filter((task)=>{return task.id !== taskid }))
+  }
+
+  const completeTask = (taskid) =>{
+    console.log(taskid)
+    setTodoList(
+      todoList.map((task)=>{
+        if (task.id === taskid) {return {...task, completed:true} 
+      } else {
+        return task;
+      }
+       })
+    );
+  };
+
 
   
   return <div className='App'>
           <div>
             <input onChange={inputTask}/>
             <button onClick={addTask}>Add Task</button>
-
-            {/* {todoList.map((task,key)=>{ return <div key={key}}>{task} <button onClick={() => {completeStatus()}}>complete</button><button onClick={()=>{deleteTask(task,key)}}>x</button></div>})} */}
+            {todoList.map((task)=>{
+              return (
+            <TaskList name={task.name} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask} />
+            )})}
           </div>
          </div>
   };
--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,10 @@
-export const Planets = (props) =>{
-    return !props.isGasPlanet && <h1>{props.name}</h1>
-}
+import React from 'react';
+
+export const TaskList = (props) =>{
+    return (
+    <div key={props.id} style={{backgroundColor:props.completed? "green":"white"}}>
+        <h1>{props.name}</h1>
+    <button onClick={()=>props.deleteTask(props.id)}>x</button>
+    <button onClick={()=>props.completeTask(props.id)}>Completed</button>
+    </div>)
+ };
