4dbc010f21047863d5c6ab552ccd41bb64fd091d
flybirdgroup <flybirdgroup@163.com>
2022-12-03 09:51:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import {useState}from "react";
 
 
 const App = () => {
-  const [todoList,setTodoList] = useState([''])
+  const [todoList,setTodoList] = useState([])
   const [newTask,setNewTask] = useState("")
+  const deleteTask =(taskname,index) =>{
+    setTodoList(todoList.filter((task,key)=>{return (task,key) !== (taskname,index)}))
+  }
   
   return <div className='App'>
           <div>
             <input onChange={(event) =>{setNewTask(event.target.value)}}/>
             <button onClick={() =>{setTodoList([...todoList,newTask])}}>Add Task</button>
-            {todoList.map((task,key)=>{ return <div key={key}>{task}</div>})}
+
+            {todoList.map((task,key)=>{ return <div key={key}>{task} <button onClick={()=>{deleteTask(task,key)}}>x</button></div>})}
           </div>
          </div>
   
