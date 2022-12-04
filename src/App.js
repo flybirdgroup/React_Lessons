15cff2eaf96f2333c2ff89aed76179e49e82a79b
flybirdgroup <flybirdgroup@163.com>
2022-12-04 17:16:49 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React from 'react';
 import './App.css';
-import Axios from "axios"
-import {useState, useEffect }from "react";
+// import Axios from "axios"
+// import {useState, useEffect }from "react";
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { Menu } from './pages/Menu';
+import { Navbar } from './pages/Navbar';
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 // import {Text} from "./Utils"
 
 
 const App = () => {
-  // fetch("https://catfact.ninja/fact").then((res)=>res.json()).then((data)=>{
-  // console.log(data);
-  // });
-  const [name,setName] = useState("")
-  // const [predictedAge,SetPredictedAge] = useState('');
-
-  const fetchData = (name)=>{
-    Axios.get(`https://uselessfacts.jsph.pl/${name}.json`).then((res)=>
-    {console.log(res.data) || setName(res.data.text)})
-  };
-
-
-  useEffect(()=>{
-    fetchData();}
-    ,[]
-    );
-
-  return <div className='App'>
-          <h1>Generate Exerise</h1>
-          <button onClick={() =>fetchData('random')}>
-              Party
-            </button>
-            <button onClick={() =>fetchData('today')}>
-              Family
-            </button>
-            <button onClick={() =>fetchData('random')}>
-              Office
-            </button>
-          <p>{name}</p>
+  
+  return (<div className='App'>
+          <Router>
+            <Navbar />
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+            </Routes>
+          </Router>
           </div>
+          )
   };
 
 export default App
---
+++ src/pages/Menu.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Menu =() =>{
+    return <h1>This is the menu page</h1>
+}
