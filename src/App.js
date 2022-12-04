2767af9dc5e84ff8bdcf0cd76ef812d1b6aeb64c
flybirdgroup <flybirdgroup@163.com>
2022-12-04 12:51:10 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ const App = () => {
   // fetch("https://catfact.ninja/fact").then((res)=>res.json()).then((data)=>{
   // console.log(data);
   // });
-  const fetchCatFact = ()=>{
-    Axios.get("https://catfact.ninja/fact").then((res)=>{setCatFact(res.data.fact)})
+  const [name,setName] = useState("")
+  const [predictedAge,SetPredictedAge] = useState(null);
+  const fetchData = ()=>{
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>
+    {SetPredictedAge(res.data) || console.log(res.data)})
   };
 
-  const [catFact,setCatFact] = useState("")
   useEffect(()=>{
-    fetchCatFact();}
+    fetchData();}
     ,[]
     );
 
   return <div className='App'>
-          <button onClick={fetchCatFact}>
-              Generate Cat Fact
+          <input placeholder="Ex. Pedro..." onChange={(event)=>{setName(event.target.value)}}/>
+          <button onClick={fetchData}>
+              Predict Age
             </button>
-           <p>{catFact}</p>
+           <h1>Predicted Name: {predictedAge?.name}</h1>
+           <h1>Predicted Age: {predictedAge?.age}</h1>
+           <h1>Predicted Count: {predictedAge?.count}</h1>
+
           </div>
   };
 
