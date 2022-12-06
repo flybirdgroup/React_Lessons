ce6cd9033cc643fc6e0dbb447a15df718ee29c67
flybirdgroup <flybirdgroup@163.com>
2022-12-06 08:57:03 +0800

    updated

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import {useForm} from 'react-hook-form';
+import React from "react";
+
+export const Form = ()=>{
+    const {register, handleSubmit} = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Full Name...." {...register("fullName")}/>
+            <input type="text" placeholder="Email...." {...register("email")}/>
+            <input type="text" placeholder="Age...." {...register("age")}/>
+            <input type="password" placeholder="Password...." {...register("password")}/>
+            <input type="password" placeholder="Confirm Password...." {...register("confirmpassword")}/>
+            <input type="submit" />
+        </form>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { Form } from "../components/Form";
+
+
 
 export const Home =() =>{
     const {username} = useContext(AppContext)
-    return <h1>This is the home page and user is {username}</h1>
+    return (
+        <div>
+         <h1>This is the home page and user is {username}</h1>
+           <Form />
+           </div>
+           )
 }
