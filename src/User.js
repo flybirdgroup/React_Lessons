60100a388b6dcf4ecf162698c893be21ed6c65c2
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2022-12-06 10:17:43 +0800

    updated

--- src/User.js
+++
@@ -1,6 +0,0 @@
-export const User =(props) =>{
-    return (<div>
-      <h1 style={{color:"green"}}>{props.name} {props.age}</h1>
-    </div>);
-  };
-  
--- src/Utils.js
+++
@@ -1,3 +0,0 @@
-export const Planets = (props) =>{
-    return !props.isGasPlanet && <h1>{props.name}</h1>
-}
