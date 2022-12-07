10d98fc8ddacc1737530a30f7cb716e803fd4d13
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2022-12-07 09:17:38 +0800

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './App.css';
 // import Axios from "axios"
-// import {useState, createContext }from "react";
-import { Form } from './components/Form';
+// import {useState }from "react";
+// import { Form } from './components/Form';
 // import {Text} from "./Utils"
+import {useToogle} from "./useToggle"
 
 
 // export const AppContext = createContext();
 
 const App = () => {
-  // const [username, setUsername] = useState("Michael");
+  const [isVisible, toggle] = useToogle(false);
+  const [isVisible2, toggle2] = useToogle(false);
 
   return (<div className='App'>
-          <Form />
+          <button onClick={toggle}>
+            {isVisible ? "Hide":"Show"}
+          </button>
+          {isVisible && <h1>Hidden Text</h1>}
+          <p></p>
+
+          <button onClick={toggle2}>
+            {isVisible2 ? "Hide":"Show"}
+          </button>
+          {isVisible2 && <h1>Hidden Text</h1>}
           </div>
           )
   };
